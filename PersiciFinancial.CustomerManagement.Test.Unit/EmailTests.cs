3e326ca4045a6594d22411be6decb642102a62c0
aryan1987 <aryanpour.dev@gmail.com>
2022-06-15 07:58:05 +0200

    This_is_a_test_for_merge

---
+++ PersiciFinancial.CustomerManagement.Test.Unit/EmailTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace PersiciFinancial.CustomerManagement.Test.Unit
+{
+    public class EmailTests
+    {
+        [Fact]
+        public void This_is_a_test_for_merge()
+        {
+
+        }
+    }
+}
